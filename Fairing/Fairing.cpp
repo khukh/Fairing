dac74c488f39b44c4ec95c7d3daa934125e14170
olga.xuxrina <olga.xuxrina@mail.ru>
2020-08-31 20:02:16 +0300

    -before changing Arr

--- Fairing/Fairing.cpp
+++ Fairing/Fairing.cpp
@@ -169,78 +169,78 @@
 			double dWz = 1 * 0.05;
 			double dPitch = PITCH0 * 0.05;
 			double rund = dis(gen);
-			#pragma omp parallel for private(b) num_threads(4)
-			for (int i = 0; i < 0; i++) {
-				//std::string count = std::to_string(i);
-				//std::string filename = "res_varAtm" + std::to_string(1)+".txt";
-				//std::ofstream fout24(filename);
-				double n = omp_get_thread_num();				
-				double dVx, dVy, dVz, dWwx, dWwy, dWwz, ddPitch, windX, windZ;				
-				#pragma omp critical
-				{				
-					rund = dis(gen);
-					dVz = dV * 0.5 * dis(gen);
-					dVy = sqrt(dV*dV - dVz * dVz) * dis(gen);
-					dVx = sqrt(dV*dV - dVz * dVz - dVy * dVy)*dis(gen);
-					
-					dWwz = 0;
-					dWwz=/*dWz * dis(gen)*/0;
-					dWwy = /*1E-1 * dis(gen)*/0;
-					dWwx = /*1E-1 * dis(gen)*/0;
-					ddPitch = 0/*dPitch * dis(gen)*/;
-					windX = 0 * dis(gen);
-					windZ = sqrt(0 * 0 - windX * windX)/**dis(gen)*/;
-
-					//f[9] = 1 + ddPitch;
-					/*f[2] = b[2] + dVz;
-					f[1] = b[1] + dVy;
-					f[0] = b[0] + dVx;
-					f[8] = b[8] + dWwz;
-					f[6] = b[6] + dWwx;
-					f[7] = b[7] + dWwy;*/
-					b = { V1 + dVx,V2 + dVy,V3 + dVz,/**/0,r + 80E+3,0,/**/0 + dWwx,0 + dWwy,-1 + dWwz,/**/PITCH0 + ddPitch, YAW0, ROLL0,0,M0 };
-					//std::cout << b[0] << '\t' << i << '\n';
-				}
-
-
-				Drop buf3(b);
-				Drop Rocket3(b);
-				//distrFull buf3(b, windX, windZ);
-				//distrFull Rocket3(b, windX, windZ);
-				Rocket3.setStageParam(30000, 0, 0, Smm, 0, Ll, 0, Ix1, Iy1, Iz1, I_XY0);
-				Rocket3.addErotation(LAT, AZIM);
-				Rocket3.nonIntegr();
-				buf3.setStageParam(30000, 0, 0, Smm, 0, Ll, 0, Ix1, Iy1, Iz1, I_XY0);
-				buf3.addErotation(LAT, AZIM);
-				buf3.nonIntegr();
-				dropInteg(Rocket3, buf3);
-#pragma omp critical
-				{				
-					std::cout << i << '\t' << i << '\n';
-					fout3 << i << '\t' << dVx << '\t' << dVy << '\t' << dVz << '\t' << ddPitch << '\t' << dWwz << '\t' << windX << '\t' << windZ << '\t' << '\t' << b[0] << '\t' << b[1] << '\t' << b[2] << '\t' << ddPitch << '\t' << dWwx << '\t' << dWwy << '\t' << dWwz << '\t' << windX << '\t' << windZ << '\n';
-				fout4 << i << '\t';
-				if (Rocket3.getParam().vect[13] < 999) {
-					Rocket3.printParam(fout4);
-				}
-				else {
-					fout4 << i << '\n';
-					//flag = 0;
-				}
-					
-				}
-				////static int i = 0;
-				////i++;
-				
-				
-				//b[9] -= ddPitch;
-				//b[2] -= dVz;
-				//b[1] -= dVy;
-				//b[0] -= dVx;
-				//b[8] -= dWwz;
-				//b[6] -= dWwx;
-				//b[7] -= dWwy;
-			
-			}
+//			#pragma omp parallel for private(b) num_threads(4)
+//			for (int i = 0; i < 0; i++) {
+//				//std::string count = std::to_string(i);
+//				//std::string filename = "res_varAtm" + std::to_string(1)+".txt";
+//				//std::ofstream fout24(filename);
+//				double n = omp_get_thread_num();				
+//				double dVx, dVy, dVz, dWwx, dWwy, dWwz, ddPitch, windX, windZ;				
+//				#pragma omp critical
+//				{				
+//					rund = dis(gen);
+//					dVz = dV * 0.5 * dis(gen);
+//					dVy = sqrt(dV*dV - dVz * dVz) * dis(gen);
+//					dVx = sqrt(dV*dV - dVz * dVz - dVy * dVy)*dis(gen);
+//					
+//					dWwz = 0;
+//					dWwz=/*dWz * dis(gen)*/0;
+//					dWwy = /*1E-1 * dis(gen)*/0;
+//					dWwx = /*1E-1 * dis(gen)*/0;
+//					ddPitch = 0/*dPitch * dis(gen)*/;
+//					windX = 0 * dis(gen);
+//					windZ = sqrt(0 * 0 - windX * windX)/**dis(gen)*/;
+//
+//					//f[9] = 1 + ddPitch;
+//					/*f[2] = b[2] + dVz;
+//					f[1] = b[1] + dVy;
+//					f[0] = b[0] + dVx;
+//					f[8] = b[8] + dWwz;
+//					f[6] = b[6] + dWwx;
+//					f[7] = b[7] + dWwy;*/
+//					b = { V1 + dVx,V2 + dVy,V3 + dVz,/**/0,r + 80E+3,0,/**/0 + dWwx,0 + dWwy,-1 + dWwz,/**/PITCH0 + ddPitch, YAW0, ROLL0,0,M0 };
+//					//std::cout << b[0] << '\t' << i << '\n';
+//				}
+//
+//
+//				Drop buf3(b);
+//				Drop Rocket3(b);
+//				//distrFull buf3(b, windX, windZ);
+//				//distrFull Rocket3(b, windX, windZ);
+//				Rocket3.setStageParam(30000, 0, 0, Smm, 0, Ll, 0, Ix1, Iy1, Iz1, I_XY0);
+//				Rocket3.addErotation(LAT, AZIM);
+//				Rocket3.nonIntegr();
+//				buf3.setStageParam(30000, 0, 0, Smm, 0, Ll, 0, Ix1, Iy1, Iz1, I_XY0);
+//				buf3.addErotation(LAT, AZIM);
+//				buf3.nonIntegr();
+//				dropInteg(Rocket3, buf3);
+//#pragma omp critical
+//				{				
+//					std::cout << i << '\t' << i << '\n';
+//					fout3 << i << '\t' << dVx << '\t' << dVy << '\t' << dVz << '\t' << ddPitch << '\t' << dWwz << '\t' << windX << '\t' << windZ << '\t' << '\t' << b[0] << '\t' << b[1] << '\t' << b[2] << '\t' << ddPitch << '\t' << dWwx << '\t' << dWwy << '\t' << dWwz << '\t' << windX << '\t' << windZ << '\n';
+//				fout4 << i << '\t';
+//				if (Rocket3.getParam().vect[13] < 999) {
+//					Rocket3.printParam(fout4);
+//				}
+//				else {
+//					fout4 << i << '\n';
+//					//flag = 0;
+//				}
+//					
+//				}
+//				////static int i = 0;
+//				////i++;
+//				
+//				
+//				//b[9] -= ddPitch;
+//				//b[2] -= dVz;
+//				//b[1] -= dVy;
+//				//b[0] -= dVx;
+//				//b[8] -= dWwz;
+//				//b[6] -= dWwx;
+//				//b[7] -= dWwy;
+//			
+//			}
 		//	std::cout << b[0];
 		//for (int i = 0; i < 100; i++) {
 		//	fout3 << 4.5*i/100 << '\t';
